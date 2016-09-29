46069f76d29cf7300aa8eeacb60c4a90ab91ccc9
luigi-asprino <luigi.asprino@gmail.com>
2016-09-29 16:41:36 +0200

    Renaming

--- lizard.jetty/src/main/java/it/cnr/istc/stlab/lizard/jetty/JettyServer.java
+++ lizard.jetty/src/main/java/it/cnr/istc/stlab/lizard/jetty/JettyServer.java
@@ -6,12 +6,13 @@ import org.eclipse.jetty.servlet.ServletHolder;
 
 public class JettyServer {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
+
+		Server jettyServer = new Server(8080);
+
 		ServletContextHandler context = new ServletContextHandler(
 				ServletContextHandler.SESSIONS);
 		context.setContextPath("/");
-
-		Server jettyServer = new Server(8080);
 		jettyServer.setHandler(context);
 
 		ServletHolder jerseyServlet = context.addServlet(
@@ -21,11 +22,13 @@ public class JettyServer {
 		// Tells the Jersey Servlet which REST service/class to load.
 		jerseyServlet.setInitParameter(
 				"jersey.config.server.provider.classnames",
-				LizardServlet.class.getCanonicalName());
+				Lizard.class.getCanonicalName());
 
 		try {
 			jettyServer.start();
 			jettyServer.join();
+		} catch (Exception e) {
+			e.printStackTrace();
 		} finally {
 			jettyServer.destroy();
 		}
--- lizard.jetty/src/main/java/it/cnr/istc/stlab/lizard/jetty/Lizard.java
+++ lizard.jetty/src/main/java/it/cnr/istc/stlab/lizard/jetty/Lizard.java
@@ -13,7 +13,7 @@ import javax.ws.rs.core.Response;
 import org.json.JSONObject;
 
 @Path("/lizard")
-public class LizardServlet {
+public class Lizard {
 
 	/**
 	 * 
